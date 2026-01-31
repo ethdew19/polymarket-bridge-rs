7eb65bbeae095169b39ccffc9248bb64fd83a3fe
Ethan <ethdew19@gmail.com>
2026-01-31 16:36:25 -0600

    add endpoints

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,16 @@
 use crate::{
     error::{RestError, Result},
-    types::ToQueryParams,
+    types::{
+        CreateDepositAddressesRequest, CreateDepositAddressesResponse,
+        CreateWithdrawalAddressesRequest, CreateWithdrawalAddressesResponse, GetQuoteRequest,
+        GetQuoteResponse, GetSupportedAssetsResponse, GetTransactionStatusRequest,
+        GetTransactionStatusResponse, ToQueryParams,
+    },
 };
 use reqwest::{Client, Method, Request};
 use serde::{Serialize, de::DeserializeOwned};
 
-const BASE_URL: &str = "https://data-api.polymarket.com";
+const BASE_URL: &str = "https://bridge.polymarket.com";
 
 pub struct BridgeClient {
     pub client: Client,
@@ -55,14 +60,30 @@ impl BridgeClient {
         Ok(response)
     }
 
-    async fn post(&self) {}
-
-    async fn get<Req: Serialize, Res: DeserializeOwned + Serialize>(
+    async fn post<Body: Serialize, Res: DeserializeOwned + Serialize>(
         &self,
         path: &str,
-        req: &Req,
+        body: &Body,
     ) -> Result<Res> {
-        let parms = req.query_params();
+        let path = format!("{}/{}", self.base_url, path);
+        println!("{:?}", path);
+        let request = self
+            .client
+            .request(Method::POST, path)
+            .json(body)
+            .build()
+            .map_err(RestError::RequestError)?;
+        self.request(request).await
+    }
+
+    async fn get<Params: Serialize, Res: DeserializeOwned + Serialize>(
+        &self,
+        path: &str,
+        params: &Params,
+    ) -> Result<Res> {
+        let parms = params.query_params();
+        let path = format!("{}/{}{}", self.base_url, path, parms);
+        println!("{:?}", path);
         let request = self
             .client
             .request(Method::GET, path)
@@ -71,8 +92,116 @@ impl BridgeClient {
         self.request(request).await
     }
 
-    pub async fn get_supported_assets(&self) -> Result<()> {
-        Ok(())
-        //self.get("");
+    pub async fn get_supported_assets(&self) -> Result<GetSupportedAssetsResponse> {
+        self.get("supported-assets", &()).await
+    }
+
+    pub async fn get_quote(&self, args: &GetQuoteRequest) -> Result<GetQuoteResponse> {
+        self.post("quote", args).await
+    }
+
+    pub async fn create_deposit_addresses(
+        &self,
+        args: &CreateDepositAddressesRequest,
+    ) -> Result<CreateDepositAddressesResponse> {
+        self.post("deposit", args).await
+    }
+
+    pub async fn create_withdrawal_addresses(
+        &self,
+        args: &CreateWithdrawalAddressesRequest,
+    ) -> Result<CreateWithdrawalAddressesResponse> {
+        self.post("withdraw", args).await
+    }
+
+    pub async fn get_transaction_status(
+        &self,
+        args: &GetTransactionStatusRequest,
+    ) -> Result<GetTransactionStatusResponse> {
+        self.get(&format!("status/{}", args.address), &()).await
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_supported_assets() {
+        let client = BridgeClient::default();
+
+        let response = client.get_supported_assets().await;
+
+        println!("{:?}", response);
+
+        assert!(response.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_get_quote() {
+        let client = BridgeClient::default();
+
+        let args = &GetQuoteRequest {
+            from_amount_base_unit: 10000000.to_string(),
+            from_chain_id: 1.to_string(),
+            from_token_address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".to_string(),
+            recipient_address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".to_string(),
+            to_chain_id: 10.to_string(),
+            to_token_address: "0x7F5c764cBc14f9669B88837ca1490cCa17c31607".to_string(),
+        };
+
+        let response = client.get_quote(args).await;
+
+        println!("{:?}", response);
+
+        assert!(response.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_create_deposit_addresses() {
+        let client = BridgeClient::default();
+
+        let args = &CreateDepositAddressesRequest {
+            address: "0x56687bf447db6ffa42ffe2204a05edaa20f55839".to_string(),
+        };
+        let response = client.create_deposit_addresses(args).await;
+
+        println!("{:?}", response);
+
+        assert!(response.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_create_withdrawal_addresses() {
+        let client = BridgeClient::default();
+
+        let args = &CreateWithdrawalAddressesRequest {
+            address: "0x56687bf447db6ffa42ffe2204a05edaa20f55839".to_string(),
+            to_chain_id: 1.to_string(),
+            to_token_address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".to_string(),
+            recipient_addr: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045".to_string(),
+        };
+
+        let response = client.create_withdrawal_addresses(args).await;
+
+        println!("{:?}", response);
+
+        assert!(response.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_get_transaction_status() {
+        let client = BridgeClient::default();
+
+        let args = &GetTransactionStatusRequest {
+            address: "bc1qs82vw5pczv9uj44n4npscldkdjgfjqu7x9mlna".to_string(),
+        };
+
+        let response = client.get_transaction_status(args).await;
+
+        println!("{:?}", response);
+
+        assert!(response.is_ok());
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
 /// Trait for converting request types to URL query parameters.
 ///
@@ -22,3 +22,122 @@ pub trait ToQueryParams: Serialize {
 }
 
 impl<T: Serialize> ToQueryParams for T {}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct GetSupportedAssetsResponse {
+    supported_assets: Vec<SupportedAsset>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct SupportedAsset {
+    //todo: string as u32
+    pub chain_id: String,
+    pub chain_name: String,
+    pub token: Token,
+    pub min_checkout_usd: f64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Token {
+    pub name: String,
+    pub symbol: String,
+    pub address: String,
+    pub decimals: u32,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct GetQuoteRequest {
+    pub from_amount_base_unit: String,
+    pub from_chain_id: String,
+    pub from_token_address: String,
+    pub recipient_address: String,
+    pub to_chain_id: String,
+    pub to_token_address: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct GetQuoteResponse {
+    pub est_checkout_time_ms: u64,
+    pub est_fee_breakdown: FeeBreakdown,
+    pub est_input_usd: f64,
+    pub est_output_usd: f64,
+    pub est_to_token_base_unit: String,
+    pub quote_id: String,
+}
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct FeeBreakdown {
+    pub app_fee_label: String,
+    pub app_fee_percent: f64,
+    pub app_fee_usd: f64,
+    pub fill_cost_percent: f64,
+    pub fill_cost_usd: f64,
+    pub gas_usd: f64,
+    pub max_slippage: f64,
+    pub min_received: f64,
+    pub swap_impact: f64,
+    pub swap_impact_usd: f64,
+    pub total_impact: f64,
+    pub total_impact_usd: f64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CreateDepositAddressesRequest {
+    pub address: String,
+}
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CreateDepositAddressesResponse {
+    pub note: String,
+    pub address: CrossChainAddress,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CrossChainAddress {
+    pub evm: String,
+    pub svm: String,
+    pub btc: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct CreateWithdrawalAddressesRequest {
+    pub address: String,
+    pub to_chain_id: String,
+    pub to_token_address: String,
+    pub recipient_addr: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct CreateWithdrawalAddressesResponse {
+    pub address: CrossChainAddress,
+    pub note: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct GetTransactionStatusRequest {
+    #[serde(skip_serializing)]
+    pub address: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct GetTransactionStatusResponse {
+    pub transactions: Vec<DepositTransaction>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct DepositTransaction {
+    pub from_chain_id: String,
+    pub from_token_address: String,
+    pub from_amount_base_unit: String,
+    pub to_chain_id: String,
+    pub to_token_address: String,
+    pub status: String,
+    pub tx_hash: String,
+    pub created_time_ms: u64,
+}
